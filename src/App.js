7487f15e4bba8fc18b4a65998d296f2dc41dc21e
elan rasabi <elan.rasabi@ncr.com>
2021-05-11 22:04:23 -0400

    App.js: begining of React root component loaded from root index.html

---
+++ src/App.js
@@ -0,0 +1,12 @@
+// App.js: begining of React root component loaded from root index.html
+
+import React from 'react';
+// export default App = <h1>Hello World</h1>;
+
+function App() {
+  return (
+      <h1>Hello World</h1>
+  );
+}
+
+export default App;
