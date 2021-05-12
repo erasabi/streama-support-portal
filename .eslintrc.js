9ce19148c071a0246d4c7955f1aee2397e22928d
elan rasabi <elan.rasabi@ncr.com>
2021-05-11 22:08:42 -0400

    .eslintrc.js: eslint config file

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+// .eslintrc.js: eslint config file
+
+module.exports = {
+    // tells ESLint that we’re working in the browser environment
+    //      Otherwise, it will incorrectly warn us that global variables like document are not defined.
+    "env": {
+        "browser": true
+    },
+    // tells ESLint that we’re using React
+    "plugins": [ "react" ],
+    "extends": [
+      "eslint:recommended",
+      "plugin:react/recommended"
+    ],
+    // tells ESLint that we’re using Babel
+    "parser": "babel-eslint"
+};
