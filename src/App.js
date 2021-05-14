684c2c61eb873d4abe1f1ffc943110ede8d94df1
elan rasabi <elan.rasabi@ncr.com>
2021-05-14 02:53:13 -0400

    refactoring

--- src/App.js
+++ src/App.js
@@ -5,30 +5,21 @@ import React from 'react';
 function App() {
   return (
     <React.Fragment>
-      <div className="header clearfix">
+      <div className="header">
         <div className="row">
           <div className="col-12 topnav">
             <img className="topnav-logo" src={"https://freepngimg.com/thumb/arrow/7-2-arrow-png-picture-thumb.png"} />
-              {/* <ul>
-                <li><a href="#news">News</a></li>
-                <li><a href="#contact">Contact</a></li>
-                <li><a href="#about">About</a></li>
-              </ul> */}
           </div>
         </div>
       </div>
-      <div className="row " >
-        <div className="col-12">
-          <div className="form-container">
-              <div className="col-9">
-                <input type="text" name="search" placeholder="Name the movie or show you'd like..." />
-              </div>
-              <div className="col-3 button-padding">
-                <input type="button" value="Request"></input>
-              </div>
-          </div>
+      <form className="row form-container" >
+        <div className="col-s-8 col-10 search-padding">
+          <input type="text" name="search" placeholder="Name the movie or show you'd like..." />
         </div>
-      </div>
+        <div className="col-s-4 col-2 button-padding">
+          <input type="button" value="Request"></input>
+        </div>
+      </form>
     </React.Fragment>
   );
 }
