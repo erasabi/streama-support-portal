6cce20513be33c6bf9b4bbefee97082c60d96670
Elan Rasabi <elan.rasabi@tinroofsoftware.com>
2021-09-20 11:10:55 -0400

    backend content

---
+++ backend/server/routes/requests.js
@@ -0,0 +1,45 @@
+// requests.js
+
+var express = require('express');
+var router = express.Router();
+var db = require('../database');
+
+router.get("/all", function(req, res) {
+    db.Request.findAll()
+        .then( requests => {
+            res.status(200).send(JSON.stringify(requests));
+        })
+        .catch( err => {
+            res.status(500).send(JSON.stringify(err));
+        });
+});
+
+router.put("/", function(req, res) {
+    db.Request.create({
+        id: req.body.id,
+        title: req.body.title,
+        posterPath: req.body.posterPath
+    })
+        .then( person => {
+            res.status(200).send(JSON.stringify(person));
+        })
+        .catch( err => {
+            res.status(500).send(JSON.stringify(err));
+        });
+});
+
+// router.delete("/:id", function(req, res) {
+//     db.Request.destroy({
+//         where: {
+//             id: req.params.id
+//         }
+//     })
+//         .then( () => {
+//             res.status(200).send();
+//         })
+//         .catch( err => {
+//             res.status(500).send(JSON.stringify(err));
+//         });
+// });
+
+module.exports = router;
