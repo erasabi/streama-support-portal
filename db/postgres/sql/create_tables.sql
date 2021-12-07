9d1c4dd78e8226e8bbeeb69c0116992ca57c7c48
Elan Rasabi <elan.rasabi@tinroofsoftware.com>
2021-12-06 19:52:35 -0500

    updated Postgres version and its tables

--- db/postgres/sql/create_tables.sql
+++ db/postgres/sql/create_tables.sql
@@ -1,12 +1,16 @@
 -- Creation of product table
 
-CREATE TABLE IF NOT EXISTS  Requests (
+CREATE TABLE IF NOT EXISTS  "Requests" (
   id VARCHAR (50) UNIQUE NOT NULL,
   title VARCHAR (100) NOT NULL,
-  posterPath VARCHAR (255),
-  createdAt TIMESTAMP WITH TIME ZONE,
-  updatedAt TIMESTAMP WITH TIME ZONE,
-  originalTitle VARCHAR ( 120 ),
-  releaseDate VARCHAR ( 20 ), 
+  "posterPath" VARCHAR (255),
+  "createdAt" TIMESTAMP WITH TIME ZONE,
+  "updatedAt" TIMESTAMP WITH TIME ZONE,
+  "originalTitle" VARCHAR (120),
+  "releaseDate" VARCHAR (20), 
   adult BOOLEAN,
+  "mediaType" VARCHAR(20),
+  "queueStatus" VARCHAR(20),
+  "queueMessage" VARCHAR(20),
+  "requestUser" VARCHAR(60)
 );
