661bced6ef07a102f3be5a2bf92dd1054d3f0586
Elan Rasabi <elan.rasabi@tinroofsoftware.com>
2021-12-10 19:55:42 -0500

    changed file structure: swapped backend/frontend for server/client

--- server/database.js
+++ server/database.js
@@ -3,7 +3,7 @@ const sequelize = new Sequelize(process.env.DB_SCHEMA || 'postgres',
     process.env.DB_USER || 'postgres',
     process.env.DB_PASSWORD || '',
     {
-        host: process.env.DB_HOST || 'localhost',
+        host: process.env.DB_HOST || '127.0.0.1',
         port: process.env.DB_PORT || 5432,
         dialect: 'postgres',
         dialectOptions: {
---
+++ server/models/requests.js
@@ -0,0 +1,61 @@
+'use strict';
+const {
+  Model
+} = require('Datatype');
+module.exports = (Datatype, DataTypes) => {
+  class Requests extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Datatype lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  Requests.init({
+    id: {
+      type: Datatype.STRING(50),
+      unique: true,
+      allowNull: false
+    },
+    title: {
+      type: Datatype.STRING(100),
+      allowNull: false
+    },
+    "posterPath": {
+      type: Datatype.STRING(255)
+    },
+    "createdAt": { 
+      type: Datatype.DATE
+    },
+    "updatedAt": {
+      type: Datatype.DATE
+    },
+    "originalTitle": {
+      type: Datatype.STRING(120)
+    },
+    "releaseDate": {
+      type: Datatype.STRING(20)
+    },
+    adult: {
+      type: Datatype.BOOLEAN
+    },
+    "mediaType": {
+      type: Datatype.STRING(20)
+    },
+    "queueStatus": {
+      type: Datatype.STRING(20)
+    },
+    "queueMessage": {
+      type: Datatype.STRING(20)
+    },
+    "requestUser": {
+      type: Datatype.STRING(60)
+    }
+  }, {
+    Datatype,
+    modelName: 'Requests',
+  });
+  return Requests;
+};
