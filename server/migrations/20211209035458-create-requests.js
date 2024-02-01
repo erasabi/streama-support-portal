aebf600e66e1cefc2a0f50ed23dc8c77acdc144e
erasabi <elanrasabi80@gmail.com>
2024-01-31 20:33:13 -0500

    added support for queueEvents

--- server/migrations/20211209035458-create-requests.js
+++ server/migrations/20211209035458-create-requests.js
@@ -1,50 +1,58 @@
-'use strict';
+"use strict"
 
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('Requests', {
-      id: { 
-        type: Sequelize.STRING(50),
-        unique: true, 
-        allowNull: false 
-      },
-      title: { 
-        type: Sequelize.STRING(100),
-        allowNull: false 
-      },
-      "posterPath": {
-        type: Sequelize.STRING(255)
-      },
-      "createdAt": {
-        type: Sequelize.DATE
-      },
-      "updatedAt": {
-        type: Sequelize.DATE
-      },
-      "originalTitle": {
-        type: Sequelize.STRING(120)
-      },
-      "releaseDate": {
-        type: Sequelize.STRING(20)
-      },
-      adult: {
-        type: Sequelize.BOOLEAN
-      },
-      "mediaType": {
-        type: Sequelize.STRING(20)
-      },
-      "queueStatus": {
-        type: Sequelize.STRING(40)
-      },
-      "queueMessage": {
-        type: Sequelize.STRING(120)
-      },
-      "requestUser": {
-        type: Sequelize.STRING(60)
-      }
-    });
-  },
-  down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('Requests');
-  }
-};
+	up: async (queryInterface, Sequelize) => {
+		await queryInterface.createTable("Requests", {
+			id: {
+				type: Sequelize.STRING(50),
+				unique: true,
+				allowNull: false,
+			},
+			title: {
+				type: Sequelize.STRING(100),
+				allowNull: false,
+			},
+			posterPath: {
+				type: Sequelize.STRING(255),
+			},
+			createdAt: {
+				type: Sequelize.DATE,
+			},
+			updatedAt: {
+				type: Sequelize.DATE,
+			},
+			originalTitle: {
+				type: Sequelize.STRING(120),
+			},
+			releaseDate: {
+				type: Sequelize.STRING(20),
+			},
+			adult: {
+				type: Sequelize.BOOLEAN,
+			},
+			mediaType: {
+				type: Sequelize.STRING(20),
+			},
+			queueStatus: {
+				type: Sequelize.STRING(40),
+			},
+			queueEvents: {
+				type: Sequelize.ARRAY(
+					Sequelize.JSONB({
+						type: Sequelize.STRING(50),
+						message: Sequelize.STRING(255),
+					})
+				),
+			},
+			queueMessage: {
+				type: Sequelize.STRING(120),
+			},
+			requestUser: {
+				type: Sequelize.STRING(60),
+			},
+		})
+	},
+	down: async (queryInterface, Sequelize) => {
+		await queryInterface.dropTable("Requests")
+	},
+}
--- server/models/requests.js
+++ server/models/requests.js
@@ -1,61 +1,84 @@
-'use strict';
-const {
-  Model
-} = require('Datatype');
+"use strict"
+const { Model } = require("Datatype")
 module.exports = (Datatype, DataTypes) => {
-  class Requests extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Datatype lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  };
-  Requests.init({
-    id: {
-      type: Datatype.STRING(50),
-      unique: true,
-      allowNull: false
-    },
-    title: {
-      type: Datatype.STRING(100),
-      allowNull: false
-    },
-    "posterPath": {
-      type: Datatype.STRING(255)
-    },
-    "createdAt": { 
-      type: Datatype.DATE
-    },
-    "updatedAt": {
-      type: Datatype.DATE
-    },
-    "originalTitle": {
-      type: Datatype.STRING(120)
-    },
-    "releaseDate": {
-      type: Datatype.STRING(20)
-    },
-    adult: {
-      type: Datatype.BOOLEAN
-    },
-    "mediaType": {
-      type: Datatype.STRING(20)
-    },
-    "queueStatus": {
-      type: Datatype.STRING(40)
-    },
-    "queueMessage": {
-      type: Datatype.STRING(120)
-    },
-    "requestUser": {
-      type: Datatype.STRING(60)
-    }
-  }, {
-    Datatype,
-    modelName: 'Requests',
-  });
-  return Requests;
-};
+	class Requests extends Model {
+		/**
+		 * Helper method for defining associations.
+		 * This method is not a part of Datatype lifecycle.
+		 * The `models/index` file will call this method automatically.
+		 */
+		static associate(models) {
+			// define association here
+		}
+	}
+	Requests.init(
+		{
+			id: {
+				type: Datatype.STRING(50),
+				unique: true,
+				allowNull: false,
+			},
+			title: {
+				type: Datatype.STRING(100),
+				allowNull: false,
+			},
+			posterPath: {
+				type: Datatype.STRING(255),
+			},
+			createdAt: {
+				type: Datatype.DATE,
+			},
+			updatedAt: {
+				type: Datatype.DATE,
+			},
+			originalTitle: {
+				type: Datatype.STRING(120),
+			},
+			releaseDate: {
+				type: Datatype.STRING(20),
+			},
+			adult: {
+				type: Datatype.BOOLEAN,
+			},
+			mediaType: {
+				type: Datatype.STRING(20),
+			},
+			queueStatus: {
+				type: Datatype.STRING(40),
+			},
+			queueMessage: {
+				type: Datatype.STRING(120),
+			},
+			queueEvents: {
+				type: Datatype.ARRAY(
+					Datatype.JSONB({
+						type: DataTypes.STRING(50),
+						message: DataTypes.STRING(255),
+					})
+				),
+				validate: {
+					isValidEvent(value) {
+						if (!Array.isArray(value)) {
+							throw new Error("queueEvents must be an array")
+						}
+						for (const event of value) {
+							if (!event.type || !event.message) {
+								throw new Error(
+									"Each event in queueEvents must have eventType and eventMessage"
+								)
+							}
+						}
+					},
+				},
+			},
+			requestUser: {
+				type: Datatype.STRING(60),
+			},
+		},
+		{
+			Datatype,
+			modelName: "Requests",
+		}
+	)
+	return Requests
+}
